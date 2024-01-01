7e45108f42a3699360c69a7e671e7da081cc654b
venkatesh <89520480+venki200221@users.noreply.github.com>
2024-01-01 21:33:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,10 @@
 from flask import Flask
-from flask import jsonify
-from flask import request
-
 
 app = Flask(__name__)
 
-about =  {  
-            "version" : "0.1",
-            "name"    : "dewdrop"
-        }
-
-users = []
-
-@app.route('/', methods=['GET'])
+@app.route('/')
 def hello_world():
-    return "Hello, Dew Drop Service!"
+    return 'Hello, World!'
 
-@app.route('/about', methods=['GET'])
-def returnAll():
-    return jsonify({'about' : about})
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
